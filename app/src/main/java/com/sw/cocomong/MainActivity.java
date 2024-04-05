c32c57a328ba6ef93084a81039a685f0ea686ed8
BuildTools <unconfigured@null.spigotmc.org>
2024-04-06 06:32:05 +0900

    FavoriteActivity.java 에 어댑터 등 구현

--- app/src/main/java/com/sw/cocomong/MainActivity.java
+++ app/src/main/java/com/sw/cocomong/MainActivity.java
@@ -2,6 +2,7 @@ package com.sw.cocomong;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,4 +12,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.refridge_list);
     }
+
+    // 명시적인 인텐트 구조
+    /*
+    Intent intent = new Intent(this, NextActivity.class);
+
+    @Override
+    public void startActivity(Intent intent) {
+        super.startActivity(intent);
+    }
+     */
 }
