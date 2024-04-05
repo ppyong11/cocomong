e85e88f9db8999cc0aabaf40f8ec69a6659a3a4a
BuildTools <unconfigured@null.spigotmc.org>
2024-04-06 05:07:52 +0900

    xmlName update

--- app/src/main/java/com/sw/cocomong/MainActivity.java
+++ app/src/main/java/com/sw/cocomong/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.refreger);
+        setContentView(R.layout.refridge_list);
     }
 }
