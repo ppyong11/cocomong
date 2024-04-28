63f9ea24796ef1c7455f46e8dbe8a9bfb5ad61df
daheeya <ekgmltkfkd0@naver.com>
2024-04-28 23:51:01 +0900

    LoginActivity.java

--- app/src/main/java/com/sw/cocomong/view/activity/LoginActivity.java
+++ app/src/main/java/com/sw/cocomong/view/activity/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.sw.cocomong.view.activity;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -8,6 +11,15 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.sw.cocomong.R;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
 
 public class LoginActivity extends AppCompatActivity {
     Button login;
@@ -20,12 +32,69 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.login);
 
         login = findViewById(R.id.btn_login);
-        name="dahee";
-        pw="1234";
+        name = "test";
+        pw = "1234";
 
-        login.setOnClickListener(v->{
-            Toast.makeText(LoginActivity.this, "실행성공", Toast.LENGTH_SHORT).show();
+        login.setOnClickListener(v -> {
+            try {
+
+                CustomTask task = new CustomTask();
+                String result= task.execute(name,pw).get();
+                Log.w("받은값 : ", result);
+
+                Intent intent = new Intent(LoginActivity.this,RefridgeActivity.class);
+                startActivity(intent);
+                finish();
+            } catch (Exception e){
+                e.printStackTrace();
+            }
 
         });
+    }
+
+    class CustomTask extends AsyncTask<String, Void, String> {
+        String sendMsg, receiveMsg;
+
+        @Override
+        // doInBackground의 매개변수 값이 여러개일 경우를 위해 배열로
+        protected String doInBackground(String... strings) {
+            try {
+                String str;
+                URL url = new URL("http://58.224.91.191:8080/android");  // 어떤 서버에 요청할지(localhost 안됨.)
+                // ex) http://123.456.789.10:8080/hello/android
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                conn.setRequestMethod("POST");                              //데이터를 POST 방식으로 전송합니다.
+                conn.setDoOutput(true);
+
+                // 서버에 보낼 값 포함해 요청함.
+                OutputStreamWriter osw = new OutputStreamWriter(conn.getOutputStream());
+                // sendMsg = "id=" + strings[0] + "&pw=" + strings[1]; // GET방식으로 작성해 POST로 보냄 ex) "id=admin&pwd=1234";
+                sendMsg = "id=" + name + "&pw=" + pw; // GET방식으로 작성해 POST로 보냄 ex) "id=admin&pwd=1234";
+
+                osw.write(sendMsg);                           // OutputStreamWriter에 담아 전송
+                osw.flush();
+
+                // jsp와 통신이 잘 되고, 서버에서 보낸 값 받음.
+                if (conn.getResponseCode() == conn.HTTP_OK) {
+                    InputStreamReader tmp = new InputStreamReader(conn.getInputStream(), "UTF-8");
+                    BufferedReader reader = new BufferedReader(tmp);
+                    StringBuffer buffer = new StringBuffer();
+                    while ((str = reader.readLine()) != null) {
+                        buffer.append(str);
+                    }
+                    receiveMsg = buffer.toString();
+                } else {    // 통신이 실패한 이유를 찍기위한 로그
+                    Log.i("통신 결과", conn.getResponseCode() + "에러");
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            // 서버에서 보낸 값을 리턴합니다.
+            return receiveMsg;
+        }
     }
 }
