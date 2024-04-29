0382c0e2119dfdbc993ac59328dab1e8b32709a1
greatCloud13 <138741201+greatCloud13@users.noreply.github.com>
2024-04-30 00:55:04 +0900

    로그인 반정도 완성

--- app/src/main/java/com/sw/cocomong/view/activity/LoginActivity.java
+++ app/src/main/java/com/sw/cocomong/view/activity/LoginActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -22,34 +24,55 @@ import java.net.URL;
 import java.util.concurrent.ExecutionException;
 
 public class LoginActivity extends AppCompatActivity {
-    Button login;
-    String name;
-    String pw;
+    Button login_btn;
+    Button join_btn;
+
+    EditText id_edit;
+    EditText pw_edit;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
-        login = findViewById(R.id.btn_login);
-        name = "test";
-        pw = "1234";
+        join_btn = (Button)findViewById(R.id.btn_join);
+        login_btn =(Button) findViewById(R.id.btn_login);
 
-        login.setOnClickListener(v -> {
-            try {
+        id_edit = (EditText)findViewById(R.id.editTextTextPersonName2);
+        pw_edit = (EditText)findViewById(R.id.editTextTextPassword3);
+    }
 
-                CustomTask task = new CustomTask();
-                String result= task.execute(name,pw).get();
-                Log.w("받은값 : ", result);
-
-                Intent intent = new Intent(LoginActivity.this,RefridgeActivity.class);
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_join:
+                join();
+                Intent intent = new Intent(LoginActivity.this,);
                 startActivity(intent);
                 finish();
-            } catch (Exception e){
-                e.printStackTrace();
-            }
+                break;
+            case R.id.btn_login:
+                Intent intent2 = new Intent(LoginActivity.this,);
+                startActivity(intent2);
+                finish();
+                break;
+        }
+    }
 
-        });
+    void join(){
+        Log.w("join", "join");
+        try{
+            String id = id_edit.getText().toString();
+            String pw = pw_edit.getText().toString();
+
+            CustomTask task = new CustomTask();
+            String result = task.execute(id, pw).get();
+
+            Intent intent2 = new Intent(LoginActivity.this);
+            startActivity(intent2);
+            finish();
+        }catch (Exception e){
+
+        }
     }
 
     class CustomTask extends AsyncTask<String, Void, String> {
@@ -60,7 +83,7 @@ public class LoginActivity extends AppCompatActivity {
         protected String doInBackground(String... strings) {
             try {
                 String str;
-                URL url = new URL("http://58.224.91.191:8080/android");  // 어떤 서버에 요청할지(localhost 안됨.)
+                URL url = new URL("http://58.224.91.191:8080/join");  // 어떤 서버에 요청할지(localhost 안됨.)
                 // ex) http://123.456.789.10:8080/hello/android
                 HttpURLConnection conn = (HttpURLConnection) url.openConnection();
                 conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
@@ -70,7 +93,7 @@ public class LoginActivity extends AppCompatActivity {
                 // 서버에 보낼 값 포함해 요청함.
                 OutputStreamWriter osw = new OutputStreamWriter(conn.getOutputStream());
                 // sendMsg = "id=" + strings[0] + "&pw=" + strings[1]; // GET방식으로 작성해 POST로 보냄 ex) "id=admin&pwd=1234";
-                sendMsg = "id=" + name + "&pw=" + pw; // GET방식으로 작성해 POST로 보냄 ex) "id=admin&pwd=1234";
+                sendMsg = "id=" +strings[0]+ "&pw=" +strings[1]; // GET방식으로 작성해 POST로 보냄 ex) "id=admin&pwd=1234";
 
                 osw.write(sendMsg);                           // OutputStreamWriter에 담아 전송
                 osw.flush();
